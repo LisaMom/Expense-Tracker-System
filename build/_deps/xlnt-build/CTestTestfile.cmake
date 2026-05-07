e90fdc348b00c67f9ed25b0b611adaebcf2f21ae
lisa <lisamom1280@gmail.com>
2026-05-07 17:32:32 +0700

    Remove embedded repos from index, ignore build directory

---
+++ build/_deps/xlnt-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/macbookpro/Documents/bcs-A1-c++/final-project-c++/build/_deps/xlnt-src
+# Build directory: /Users/macbookpro/Documents/bcs-A1-c++/final-project-c++/build/_deps/xlnt-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("source")
